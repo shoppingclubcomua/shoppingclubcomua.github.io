94622ebea3bd6bab71b8d1757b6bec9ac2eaad95
0x7c48 <0x7c48@gmail.com>
2018-12-28 00:09:13 +0200

    new push

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var device = new ClientJS();
+
 
 var config = {
   apiKey: "AIzaSyCXDaL_fBQ7uRPa_7zkME-miQm2Uo0HoB8",
@@ -14,9 +16,9 @@ firebase.initializeApp(config);
 
 
 var database = firebase.database();
-var messaging = firebase.messaging();
 var tokens = database.ref('tokens/');
 var emails = database.ref('emails/');
+var messaging = firebase.messaging();
 
 
 messaging.usePublicVapidKey(
@@ -37,29 +39,23 @@ Push.FCM().then(function(FCM) {
 });
 
 
+// When Push notification come in
 messaging.onMessage(function(payload) {
   console.log('Message received. ', payload);
   Push.create(payload.notification.body);
 });
 
 
-async function FCMnotificationPermission() {
-    try {
-        await messaging.requestPermission();
-        const token = await messaging.getToken();
-        console.log('token do:', token);
-
-        var device = new ClientJS();
-        var now = Date.now();
-        var date = Date(now);
-        var fingerprint = device.getFingerprint();
-        var br = JSON.stringify(device.getBrowserData());
-        var us = device.getUserAgent();
-        var mob = device.isMobile();
-        var and = device.isMobileAndroid();
-        var ios = device.isMobileIOS();
-
-        tokens.push().set({
+function makeDeviceInfo(token) {
+  var now = Date.now();
+  var date = Date(now);
+  var fingerprint = device.getFingerprint();
+  var br = JSON.stringify(device.getBrowserData());
+  var us = device.getUserAgent();
+  var mob = device.isMobile();
+  var and = device.isMobileAndroid();
+  var ios = device.isMobileIOS();
+  return {
             token: token,
             created_at: now,
             created_at_string: date.toString(),
@@ -69,12 +65,79 @@ async function FCMnotificationPermission() {
             is_mobile: mob,
             is_mobile_android: and,
             is_mobile_ios: ios,
-        });
-        return token;
-      } catch (error) {
-        console.error(error);
-      }
-  };
+        }
+};
+
+
+function setTokenSentToServer(isSent) {
+  window.localStorage.setItem('sentToServer', sent ? '1' : '0');
+};
+
+
+function isTokenSentToServer() {
+  return window.localStorage.getItem('sentToServer') === '1';
+};
+
+
+function sendTokenToServer(currentToken) {
+  if (!isTokenSentToServer()) {
+    console.log('Sending token to server...', currentToken);
+
+    var deviceInfo = makeDeviceInfo(currentToken);
+    tokens.push().set(deviceInfo);
+
+    setTokenSentToServer(true);
+  } else {
+    console.log('Token already sent to server so won\'t send it again ' +
+        'unless it changes');
+  }
+};
+
+
+function updateUIForPushPermissionRequired() {
+  messaging.requestPermission().then(function() {
+    console.log('Notification permission granted.');
+  }).catch(function(err) {
+    console.log('Unable to get permission to notify.', err);
+  });
+};
+
+
+// Get Instance ID token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken().then(function(currentToken) {
+  if (currentToken) {
+    sendTokenToServer(currentToken);
+  } else {
+    // Show permission request.
+    console.log('No Instance ID token available. Request permission to generate one.');
+    // Show permission UI.
+    updateUIForPushPermissionRequired();
+    setTokenSentToServer(false);
+  }
+}).catch(function(err) {
+  console.log('An error occurred while retrieving token. ', err);
+  setTokenSentToServer(false);
+});
+
+
+// Callback fired if Instance ID token is updated.
+messaging.onTokenRefresh(function() {
+  messaging.getToken().then(function(refreshedToken) {
+    console.log('Token refreshed.');
+    // Indicate that the new Instance ID token has not yet been sent to the
+    // app server.
+    setTokenSentToServer(false);
+    // Send Instance ID token to app server.
+    sendTokenToServer(refreshedToken);
+    // ...
+  }).catch(function(err) {
+    console.log('Unable to retrieve refreshed token ', err);
+  });
+});
+
+
+// ===========================
 
 
 function showPosition(position) {
@@ -123,5 +186,5 @@ function sendEmail() {
 
 
 // setup
-setTimeout(FCMnotificationPermission, 3000);  // 3 sec
+// setTimeout(FCMnotificationPermission, 3000);  // 3 sec
 setTimeout(getLocation, 5000);                // 5 sec
