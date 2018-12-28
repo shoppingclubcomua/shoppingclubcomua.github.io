ecdf76627bab690f0dfe7698ac44f2a04cac989e
0x7c48 <0x7c48@gmail.com>
2018-12-28 02:36:39 +0200

    mob push

--- js/app.js
+++ js/app.js
@@ -217,7 +217,7 @@ function sendEmail() {
     };
 
     const key = new_email.replace(".", "_").replace("#", "-").replace("$", "*").replace("[", "&").replace("]", "?");
-    var emails = database.ref('emails/' + key);
+    var emails = database.ref('emails/' + key + '/' + 'version');
     emails.push().set(deviceInfo);
 
     alert(`Спасибо, ваш email ${new_email} добавлен в список рассылки, теперь вы подписаны на наши новости.`);
